e07ae2ecaf7fd38f052fb60021633e50859801e3
SQ <NONE>
2026-08-09 16:48:07 -0400

    Implement lambda waits to synchronize React state events

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -14,36 +14,33 @@ class CheckoutPage(BasePage):
     ERROR_BANNER = (By.CSS_SELECTOR, "h3[data-test='error']")
 
     def start_checkout(self):
-        btn = self.wait.until(EC.element_to_be_clickable(self.CHECKOUT_BTN))
-        btn.click()
+        btn = self.wait.until(EC.presence_of_element_located(self.CHECKOUT_BTN))
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", btn)
+        self.wait.until(EC.element_to_be_clickable(self.CHECKOUT_BTN)).click()
 
     def fill_customer_info(self, first, last, postal):
-        # Advanced QA technique: Force Selenium to wait for React to register the text
         def safe_enter(locator, text):
             field = self.wait.until(EC.element_to_be_clickable(locator))
+            self.driver.execute_script("arguments[0].scrollIntoView(true);", field)
             field.click()
             field.clear()
             field.send_keys(text)
-            # Halt execution until the DOM 'value' attribute physically matches the text
             WebDriverWait(self.driver, 5).until(
                 lambda d: d.find_element(*locator).get_attribute("value") == text
             )
 
-        # Only interact with fields if data was passed in the test
-        if first:
-            safe_enter(self.FIRST_NAME, first)
-        if last:
-            safe_enter(self.LAST_NAME, last)
-        if postal:
-            safe_enter(self.POSTAL_CODE, postal)
+        if first: safe_enter(self.FIRST_NAME, first)
+        if last: safe_enter(self.LAST_NAME, last)
+        if postal: safe_enter(self.POSTAL_CODE, postal)
         
-        # Now it is 100% safe to click continue
-        btn = self.wait.until(EC.element_to_be_clickable(self.CONTINUE_BTN))
-        btn.click()
+        btn = self.wait.until(EC.presence_of_element_located(self.CONTINUE_BTN))
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", btn)
+        self.wait.until(EC.element_to_be_clickable(self.CONTINUE_BTN)).click()
 
     def finish_checkout(self):
-        btn = self.wait.until(EC.element_to_be_clickable(self.FINISH_BTN))
-        btn.click()
+        btn = self.wait.until(EC.presence_of_element_located(self.FINISH_BTN))
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", btn)
+        self.wait.until(EC.element_to_be_clickable(self.FINISH_BTN)).click()
 
     def get_confirmation_text(self):
         return self.wait.until(EC.visibility_of_element_located(self.COMPLETE_HEADER)).text
